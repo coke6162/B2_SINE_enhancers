ac9bb1ea8a7af108c4d8f0c7dae184a1eab4a8dc
coke6162 <conor.j.kelly@colorado.edu>
2022-07-22 14:13:14 -0600

    updated

---
+++ RNAseq_BMDM/extract_top_750_IRGs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Get names for top 750 IRGs (by ascending log2FC)
+awk '(NR>1){if ($3 < 0 && $7 <= 0.05) print $0}' picc_BMDM_IFNG_4h.txt \
+| sort -k3,3rn \
+| awk '(NR<=750){print $1}' \
+> picc_BMDM_IFNG_4h_IRGs_top750_names.txt
+
+# Get coordindates for top 750 IRGs
+awk 'FNR==NR {a[$5] = $0; next} $1 in a {print a[$1]}' picc_BMDM_IFNG_4h.bed picc_BMDM_IFNG_4h_IRGs_top750_names.txt \
+| bedtools sort -i - \
+> picc_BMDM_IFNG_4h_IRGs_top750_names.bed
