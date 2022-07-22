d3af342c1b3408d9f383253b1eb2f649069dd53c
coke6162 <conor.j.kelly@colorado.edu>
2022-07-22 14:31:01 -0600

    changed library specification

--- RNAseq_BMDM/TEtranscripts_B2_bar_expression.R
+++ RNAseq_BMDM/TEtranscripts_B2_bar_expression.R
@@ -1,14 +1,5 @@
-# Load required packages
-# Credit for function: https://gist.github.com/smithdanielle/9913897
-check.packages <- function(pkg){
-    new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-    if (length(new.pkg))
-        install.packages(new.pkg, dependencies = TRUE)
-    sapply(pkg, require, character.only = TRUE)
-}
-
-packages <- c("dplyr", "tidyr", "plotrix", "ggplot2")
-check.packages(packages)
+# Load packages
+library("dplyr", "tidyr", "plotrix", "ggplot2")
 
 # Set input directory
 # This should be the directory that hosts your TEtranscripts DESeq2 results tables
