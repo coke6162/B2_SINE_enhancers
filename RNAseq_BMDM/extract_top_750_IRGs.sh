a736630e93c79ff84748d6b955c3605140e56057
coke6162 <conor.j.kelly@colorado.edu>
2022-07-24 15:03:10 -0600

    updated to include piccolo 2h and platanitis

--- RNAseq_BMDM/extract_top_750_IRGs.sh
+++ RNAseq_BMDM/extract_top_750_IRGs.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
 
+## Piccolo et al. 2017 IFNG 4h vs UT
+
 # Get names for top 750 IRGs (by ascending log2FC)
 awk '(NR>1){if ($3 < 0 && $7 <= 0.05) print $0}' picc_BMDM_IFNG_4h_vs_UT.txt \
-| sort -k3,3rn \
+| sort -k3,3n \
 | awk '(NR<=750){print $1}' \
 > picc_BMDM_IFNG_4h_vs_UT_IRGs_top750_names.txt
 
 # Get coordindates for top 750 IRGs
 awk 'FNR==NR {a[$5] = $0; next} $1 in a {print a[$1]}' picc_BMDM_IFNG_4h_vs_UT.bed picc_BMDM_IFNG_4h_vs_UT_IRGs_top750_names.txt \
 | bedtools sort -i - \
-> picc_BMDM_IFNG_4h_vs_UT_IRGs_top750_names.bed
+> picc_BMDM_IFNG_4h_vs_UT_IRGs_top750.bed
+
+## Piccolo et al. 2017 IFNG 2h vs UT
+
+# Get names for top 750 IRGs (by ascending log2FC)
+awk '(NR>1){if ($3 < 0 && $7 <= 0.05) print $0}' picc_BMDM_IFNG_2h_vs_UT.txt \
+| sort -k3,3n \
+| awk '(NR<=750){print $1}' \
+> picc_BMDM_IFNG_2h_vs_UT_IRGs_top750_names.txt
+
+# Get coordindates for top 750 IRGs
+awk 'FNR==NR {a[$5] = $0; next} $1 in a {print a[$1]}' picc_BMDM_IFNG_2h_vs_UT.bed picc_BMDM_IFNG_2h_vs_UT_IRGs_top750_names.txt \
+| bedtools sort -i - \
+> picc_BMDM_IFNG_2h_vs_UT_IRGs_top750.bed
+
+## Platanitis et al. 2019 IFNG 2h vs UT
+
+# Get names for top 750 IRGs (by ascending log2FC)
+awk '(NR>1){if ($3 < 0 && $7 <= 0.05) print $0}' plat_BMDM_IFNG_2h_vs_UT.txt \
+| sort -k3,3n \
+| awk '(NR<=750){print $1}' \
+> plat_BMDM_IFNG_2h_vs_UT_IRGs_top750_names.txt
+
+# Get coordindates for top 750 IRGs
+awk 'FNR==NR {a[$5] = $0; next} $1 in a {print a[$1]}' plat_BMDM_IFNG_2h_vs_UT.bed plat_BMDM_IFNG_2h_vs_UT_IRGs_top750_names.txt \
+| bedtools sort -i - \
+> plat_BMDM_IFNG_2h_vs_UT_IRGs_top750.bed
